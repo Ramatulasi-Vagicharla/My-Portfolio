ac643cb2b222e3b64b9ad47bd2af1542c52dd075
Rama Tulasi <156978271+20101113@users.noreply.github.com>
2024-11-12 22:03:33 +0530

    Add files via upload

---
+++ My-Portfolio-main/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Full Stack Developer", "Computer Programmer", "Innovator"],
+    typeSpeed: 100,
+    backSpeed: 100, // Corrected from 'backspeed' to 'backSpeed'
+    backDelay: 1000,
+    loop: true,
+  });
+  
