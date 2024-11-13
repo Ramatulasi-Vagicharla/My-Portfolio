afa6e4abe40f26f0d1a7fd8fc6c273c516718971
Rama <vagicharlaramatulasi@gmail.comgit config --global user.name Ramagit config --global user.email vagicharlaramatulasi@gmail.com>
2024-11-13 09:40:49 +0530

    committed

--- My-Portfolio-main/main.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Full Stack Developer", "Computer Programmer", "Innovator"],
-    typeSpeed: 100,
-    backSpeed: 100, // Corrected from 'backspeed' to 'backSpeed'
-    backDelay: 1000,
-    loop: true,
-  });
-  
